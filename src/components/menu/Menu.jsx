350d9b6c3c5dd35e9ba2d032f527e1413d907a7b
Kyle Henriksen <khenriksenbootcamp@gmail.com>
2022-05-13 12:04:02 -0700

    added linked in and git hub logo to menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -19,17 +19,55 @@ export default function menu({ menuOpen, setMenuOpen }) {
         <li onClick={() => setMenuOpen(false)}>
           <a href="#contact">Contact</a>
         </li>
+
+        <div
+          className="line"
+          style={{
+            border: "1px solid gray",
+            margin: "20px",
+            marginTop: "50px",
+          }}
+        ></div>
+        <p
+          style={{
+            marginLeft: "30px",
+            marginBottom: "40px",
+          }}
+        >
+          Professional
+        </p>
+        <li onClick={() => setMenuOpen(false)}>
+          <a href="https://github.com/starbjornx" target="_blank">
+            Github
+            <img
+              src="assets/GitHub-Logo.png"
+              style={{ width: "25%", marginLeft: "80px" }}
+            />
+          </a>
+        </li>
+        <li onClick={() => setMenuOpen(false)}>
+          <a
+            href="https://www.linkedin.com/in/kyle-henriksen-40ba4b228/"
+            target="_blank"
+          >
+            Linked-In
+            <img
+              src="assets/linkedin.png"
+              style={{ width: "10%", marginLeft: "50px" }}
+            />
+          </a>
+        </li>
         <li onClick={() => setMenuOpen(false)}>
           <a
             href="https://drive.google.com/file/d/1dzoJdJmnDJourjWzBNlZx6jhCjRmGqJg/view?usp=sharing"
             target="_blank"
           >
             Resume`
-          </a>
-        </li>
-        <li onClick={() => setMenuOpen(false)}>
-          <a href="https://github.com/starbjornx" target="_blank">
-            Github
+            <img
+              src="assets/arrowleft.png"
+              style={{ width: "10%", marginLeft: "80px", height: "55px" }}
+              className="blinkingarrow"
+            />
           </a>
         </li>
       </ul>
